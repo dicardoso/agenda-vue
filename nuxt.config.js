6de08fc625bed6bc183ac2743c4d2fec51cfca5c
Diogo Cardoso <diogomescardoso@gmail.com>
2022-05-09 11:02:43 -0300

    ajustes

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/agenda-vue/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
